ab445a198d194281e8721cb5fb1c1600fd985732
BMRicardas <Ricardas.Brazdzius@gmail.com>
2022-05-01 20:53:02 +0300

    delete unnecessary files

--- src/components/Section/Section.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import classes from "./Section.module.scss";
-
-const Section = ({ children }) => {
-  return <section className={classes["section"]}>{children}</section>;
-};
-
-export default Section;
